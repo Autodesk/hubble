79b43cd6d00a93c65bbb6d5317433999994c89b7
Patrick Lühne <patrick-github@luehne.de>
2017-10-24 16:00:22 +0200

    Implemented repository history chart.
    This adds a report that records the number of repositories in total, in
    organizations, and in user spaces every day, and a corresponding chart
    in the housekeeping tab.

---
+++ updater/reports/ReportRepositoryHistory.py
@@ -0,0 +1,50 @@
+from .ReportDaily import *
+
+# Lists the total number of repositories
+class ReportRepositoryHistory(ReportDaily):
+	def name(self):
+		return "repository-history"
+
+	def updateDailyData(self):
+		newHeader, newData = self.parseData(
+			self.executeQuery(self.query())
+		)
+		self.header = newHeader if newHeader else self.header
+		self.data.extend(newData)
+		self.truncateData(self.timeRangeTotal())
+		self.sortDataByDate()
+
+	# Collects the number of active repositories for a user type (user/organization) given time range
+	def subquery(self, userType):
+		query = '''
+			SELECT
+				COUNT(*) AS count
+			FROM
+				repositories
+				JOIN users ON repositories.owner_id = users.id
+			WHERE
+				TRUE'''
+
+		query += self.andExcludedEntities("users", "repositories")
+
+		if userType != None:
+			query += ''' AND
+				users.type = "''' + userType + '''"'''
+
+		return query
+
+	# Collects the number of repositories in total, in organizations, and in user spaces
+	def query(self):
+		query = '''
+			SELECT
+				"''' + str(self.yesterday()) + '''" AS date,
+				total.count AS total,
+				organizationSpace.count AS "in organizations",
+				userSpace.count AS "in user spaces"
+			FROM
+				(''' + self.subquery(None) + ''') AS total,
+				(''' + self.subquery("Organization") + ''') AS organizationSpace,
+				(''' + self.subquery("User") + ''') AS userSpace
+			'''
+
+		return query
