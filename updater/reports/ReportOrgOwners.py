c66ec188c3f0cb661d9b203280c05dcb1d7669a3
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2017-11-08 11:25:49 -0800

    Add page to list organization owners

---
+++ updater/reports/ReportOrgOwners.py
@@ -0,0 +1,27 @@
+from .Report import *
+
+# Lists all orgs and their owners without site admins or suspended users
+class ReportOrgOwners(Report):
+	def name(self):
+		return "org-owners"
+
+	# The data is overwritten every day, so skip reading the old data
+	def readData(self):
+		pass
+
+	def updateData(self):
+		self.header, self.data = self.parseData(
+			self.executeGHEConsole('''
+				puts "organization\towner(s)\n"
+				User.where(:type => "Organization")
+				    .where("''' + self.andExcludedEntities("login", "").replace('"', '\\\\"') + '''")
+				    .order("login")
+				    .each do |org|
+					owners = org.admins.where(:disabled => false, :gh_role => nil)
+					                   .where("''' +  self.andExcludedUsers("login", "").replace('"', '\\\\"') + '''")
+					                   .order("login")
+					                   .join(",")
+					puts "#{org.login}\t#{owners}\n"
+				end
+			''')
+		)
