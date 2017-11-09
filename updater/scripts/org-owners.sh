92f17fcdfd402ab1869086f8c2c6f7e8c3f48dee
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2017-11-09 12:36:04 -0800

    only query for admins

--- updater/scripts/org-owners.sh
+++ updater/scripts/org-owners.sh
@@ -5,18 +5,9 @@
 echo -e "organization\towner"
 
 github-env bin/runner -e production "'
-    orgs=User.all.select {
-        |u| u.type == \"Organization\" and not u.disabled and not u.suspended_at and u.login and u.gh_role.nil?
-    }.map {
-            |u|
-                [u.login, u.admins]
-    }.sort do |a, b|
-        a[0].casecmp(b[0])
-    end
-
-    orgs.each do |org|
-        org[1].each do |admin|
-            puts org[0] + \"\\t\" + admin.login
+    User.where(:type => \"Organization\").each do |o|
+        o.admins.where(:disabled => false, :gh_role => nil).each do |m|
+            puts \"#{o.login}\\t#{m.name}\\n\"
         end
     end
 '"
