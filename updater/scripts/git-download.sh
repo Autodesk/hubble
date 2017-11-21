46fb54a36807ae71092e7bf8d030e22f3675541b
Patrick Lühne <patrick-github@luehne.de>
2017-11-21 16:38:40 +0100

    Merge both eval calls
    This merges the two eval calls into one. This also makes in unnecessary
    to call tee anymore.

--- updater/scripts/git-download.sh
+++ updater/scripts/git-download.sh
@@ -14,23 +14,20 @@ function ghe_greater_equal () {
 }
 
 if ghe_greater_equal "2.11.0" ; then
-    # check the two most recent log files
-    CAT_LOG_FILE="zcat -f /var/log/github-audit.log{,.1*}"
     # The "github-audit.log" log file introduced in GHE 2.11.0 is only rolled
     # once a week. This was reported as a bug and is likely fixed in an
-    # upcoming version. In the meantime we grep for all log entries that have
-    # been written yesterday.
-    GREP_YESTERDAY="grep -F '$(date --date='yesterday' +'%b %d')'"
+    # upcoming version. In the meantime, we grep for all log entries in the two
+    # most recent log files (because the information from yesterday may or not
+    # be rotated already).
+    CAT_LOG_FILE="zcat -f /var/log/github-audit.log{,.1*} | grep -F '$(date --date='yesterday' +'%b %d')'"
 else
     # check yesterday’s log file
     CAT_LOG_FILE="zcat -f /var/log/github/audit.log.1*"
-    GREP_YESTERDAY="tee"
 fi
 
 echo -e "repository\tuser\tcloning?\trequests/day\tdownload/day [B]"
 
 eval "$CAT_LOG_FILE" |
-    eval "$GREP_YESTERDAY" |
     perl -ne 'print if s/.*"program":"upload-pack".*"repo_name":"([^"]+).*"user_login":"([^"]+).*"cloning":([^,]+).*"uploaded_bytes":([^ ]+).*/\1\t\2\t\3\t\4/' |
     sort |
     perl -ne '$S{$1} += $2 and $C{$1} += 1 if (/^(.+)\t(\d+)$/);END{printf("%s\t%i\t%i\n",$_,$C{$_},$S{$_}) for ( keys %S );}' |
