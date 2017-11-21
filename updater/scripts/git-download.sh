50743c828552976a94e7332b24ace50fa49cf15f
Patrick Lühne <pluehne@users.noreply.github.com>
2017-11-21 16:54:50 +0100

    Fix Git download script (#46)
    Let the Git download script check not only for log entries in the
    previous Git audit log but also the current one. Because of the unwanted
    log rotation change introduced in GitHub Enterprise 2.11, we can’t
    assume that the log files searched for with .1* contain yesterday’s
    information (it might still reside in the current, not-yet-rotated log).
    
    Also remove the known issue concerning the Git download report on
    GitHub Enterprise 2.11 in the hope that this bug is now fixed.

--- updater/scripts/git-download.sh
+++ updater/scripts/git-download.sh
@@ -14,21 +14,20 @@ function ghe_greater_equal () {
 }
 
 if ghe_greater_equal "2.11.0" ; then
-    LOG_FILE="/var/log/github-audit.log"
     # The "github-audit.log" log file introduced in GHE 2.11.0 is only rolled
     # once a week. This was reported as a bug and is likely fixed in an
-    # upcoming version. In the meantime we grep for all log entries that have
-    # been written yesterday.
-    GREP_YESTERDAY="grep -F '$(date --date='yesterday' +'%b %d')'"
+    # upcoming version. In the meantime, we grep for all log entries in the two
+    # most recent log files (because the information from yesterday may or not
+    # be rotated already).
+    CAT_LOG_FILE="zcat -f /var/log/github-audit.log{,.1*} | grep -F '$(date --date='yesterday' +'%b %d')'"
 else
-    LOG_FILE="/var/log/github/audit.log"
-    GREP_YESTERDAY="tee"
+    # check yesterday’s log file
+    CAT_LOG_FILE="zcat -f /var/log/github/audit.log.1*"
 fi
 
 echo -e "repository\tuser\tcloning?\trequests/day\tdownload/day [B]"
 
-zcat -f $LOG_FILE.1* |
-    eval "$GREP_YESTERDAY" |
+eval "$CAT_LOG_FILE" |
     perl -ne 'print if s/.*"program":"upload-pack".*"repo_name":"([^"]+).*"user_login":"([^"]+).*"cloning":([^,]+).*"uploaded_bytes":([^ ]+).*/\1\t\2\t\3\t\4/' |
     sort |
     perl -ne '$S{$1} += $2 and $C{$1} += 1 if (/^(.+)\t(\d+)$/);END{printf("%s\t%i\t%i\n",$_,$C{$_},$S{$_}) for ( keys %S );}' |
