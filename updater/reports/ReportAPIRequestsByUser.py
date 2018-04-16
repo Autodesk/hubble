4ebd6eb6a30d9816068ed842b90eddb89a67bcb5
David Farr <david.farr@sap.com>
2018-04-16 13:51:01 -0700

    Update API Requests By User
    Modify the report to tabulate the top API useres each day, display
    this information in housekeeping/api-requests

--- updater/reports/ReportAPIRequestsByUser.py
+++ updater/reports/ReportAPIRequestsByUser.py
@@ -10,20 +10,4 @@ class ReportAPIRequestsByUser(Report):
 		pass
 
 	def updateData(self):
-
-		self.header = [ "user", "0h", "1h", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h", "23h" ]
-
-		data = {}
-		stdin = None
-
-		with open(self.scriptPath("api-requests-by-user.sh")) as f:
-			stdin = f.read()
-
-		for row in csv.reader(self.executeScript([ "bash -s", "--", self.yesterday().strftime("%Y-%m-%d") ], stdin).decode("utf-8").splitlines()):
-			hour, user, reqs = row
-			if not user in data:
-				data[user] = {}
-			data[user][int(hour)] = reqs
-
-		for user in data:
-			self.data.append([ user ] + [ data[user][hour] if hour in data[user] else "0" for hour in range(24) ])
+		self.header, self.data = self.parseData(self.executeScript(self.scriptPath("api-requests-by-user.sh")))
--- updater/scripts/api-requests-by-user.sh
+++ updater/scripts/api-requests-by-user.sh
@@ -4,7 +4,14 @@
 # Count number of API calls per user
 #
 
-for i in {00..23}
-do
-  grep "now=\"$1T$i" /var/log/github/unicorn.log?? | grep request_category=api | grep -oP 'current_user=\K\S+' | grep -v ^nil$ | sort | uniq -c | sort -rn | head -20 | awk -v i=$i '{ printf("%s,%s,%s\n", i, $2, $1) }'
-done
+echo -e "user\trequests"
+
+zcat -f /var/log/github/unicorn.log.1* |
+    grep request_category=api |
+    grep -oP 'current_user=\K\S+' |
+    grep -v ^nil$ |
+    sort |
+    uniq -c |
+    sort -rn |
+    head -20 |
+    awk '{ printf("%s\t%s\n", $2, $1) }'
