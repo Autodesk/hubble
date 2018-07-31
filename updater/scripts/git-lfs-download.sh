0f89482e9e965c9eec7705dec78ab3608a430e82
Lars Schneider <larsxschneider@github.com>
2018-07-31 11:25:34 +0200

    git-download: handle very large Git LFS download numbers
    By default Perl uses scientific integer notation (e.g 2e+19) for very
    large integer numbers. The Python code would choke on it:
    
        ValueError: invalid literal for int() with base 10: '2e+19'
    
    Fix this by teaching Perl to always print the download number in
    decimal form.

--- updater/scripts/git-lfs-download.sh
+++ updater/scripts/git-lfs-download.sh
@@ -5,4 +5,4 @@
 
 echo "lfs traffic [B]"
 zcat -f /var/log/haproxy.log.1* |
-    LANG=C perl -ne '/\s200\s(\d+)\s-\s\-\s----\s.*GET (?:\/storage)?\/lfs\/\d+\// && {$traffic += $1}; END {print $traffic}'
+    LANG=C perl -ne 'use bigint; /\s200\s(\d+)\s-\s\-\s----\s.*GET (?:\/storage)?\/lfs\/\d+\// && {$traffic += $1}; END {print ($traffic+0)}'
