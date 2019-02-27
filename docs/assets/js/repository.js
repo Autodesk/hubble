69b35773ba219190d66d1099b2cdd7be7a6014f4
Steffen Hiller <steffen@github.com>
2019-02-27 21:30:28 +0100

    Add loading repository specific data

--- docs/assets/js/repository.js
+++ docs/assets/js/repository.js
@@ -3,9 +3,17 @@ $(window).bind('load', function()
     const queryString = new URLSearchParams(location.search);
     const nwo = queryString.get('nwo');
 
+    // Add nwo query string to all navigation links
     $('.navigation a').each(function()
     {
-        let href = $(this).attr('href');
+        const href = $(this).attr('href');
         $(this).attr('href', `${href}?nwo=${nwo}`);
+    });
+
+    // Replace all canvas data urls with repository specific urls
+    $('canvas[data-url]').each(function()
+    {
+        const dataUrl = $(this).attr('data-url');
+        $(this).attr('data-url', dataUrl.replace('/repository', `/repository/${nwo}`));
     });
 });
