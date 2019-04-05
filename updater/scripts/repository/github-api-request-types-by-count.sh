221fafcb87c66201809b3772cc457cbe300aef4a
Steffen Hiller <steffen@github.com>
2019-04-05 19:24:36 +0200

    Exclude internal api requests from api request types data

--- updater/scripts/repository/github-api-request-types-by-count.sh
+++ updater/scripts/repository/github-api-request-types-by-count.sh
@@ -9,6 +9,7 @@ echo -e "type\tcount"
 zcat -f /var/log/github/unicorn.log.1* |
     grep -F 'request_category=api' |
     grep -Fv 'remote_address=127.0.0.1' |
+    grep -Fv "controller=\"Api::Internal" |
     grep "repo=$REPOSITORY " |
     grep -oP 'route=\K\S+' |
     grep -Fvx 'nil' |
