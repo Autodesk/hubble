c615dae119085c5e07f04106110cf35ee754364b
Lars Schneider <larsxschneider@github.com>
2019-07-16 17:44:47 +0200

    add Git protocol chart
    Add a new chart to visualize what protocol users use to access Git
    repository data.

---
+++ updater/reports/ReportGitProtocol.py
@@ -0,0 +1,24 @@
+from .ReportDaily import *
+
+# Report what protocols is used to access Git repositories
+class ReportGitProtocol(ReportDaily):
+	def name(self):
+		return "git-protocol"
+
+	def updateDailyData(self):
+		_, items = self.parseData(
+			self.executeScript(self.scriptPath("git-protocol.sh")))
+
+		protocols = {}
+		for item in items:
+			protocols[item[0]] = item[1]
+
+		self.header = ["date", "http", "ssh"]
+		self.data.append(
+			[
+				str(self.yesterday()),
+				protocols["http"],
+				protocols["ssh"],
+			])
+		self.truncateData(self.timeRangeTotal())
+		self.sortDataByDate()
