9a0ae7776a8fab1de8451aee0f8a4e77a5fbcc41
Lars Schneider <larsxschneider@github.com>
2019-10-01 15:31:03 +0200

    count number of successful connections per protocol

--- updater/scripts/git-protocol.sh
+++ updater/scripts/git-protocol.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #
-# Count the number of connections per Git protocol
+# Count the number of successful connections per Git protocol
 #
 echo -e "Git protocol\tconnections"
 
 zcat -f /var/log/babeld/babeld.log.1* |
-	perl -ne 'print if s/.*proto=([^ ]+).*/\1/' |
+	perl -ne 'print if s/.*proto=([^ ]+).*op done.*/\1/' |
 	sort |
 	uniq -c |
 	awk '{printf("%s\t%s\n",$2,$1)}'
-
