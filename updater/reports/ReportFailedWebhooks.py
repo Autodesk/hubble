321a36ad7eef9a0dcc8177053da633e82757ba41
Stefan Stölzle <stoe@github.com>
2020-10-07 17:57:24 +0200

    🤘 Add updater for failed webhooks and historical table

---
+++ updater/reports/ReportFailedWebhooks.py
@@ -0,0 +1,17 @@
+from .ReportDaily import *
+
+# Report how many webhooks failed
+class ReportFailedWebhooks(ReportDaily):
+	def name(self):
+		return "failed-webhooks"
+
+	def readData(self):
+		pass
+
+	def updateData(self):
+		self.detailedHeader, self.detailedData = self.parseData(
+			self.executeScript(self.scriptPath("failed-webhooks.sh")))
+		self.header = ["date", "failed webhooks"]
+		self.data.append([str(self.yesterday()), len(self.detailedData)])
+		self.truncateData(self.timeRangeTotal())
+		self.sortDataByDate()
--- updater/scripts/failed-webhooks.sh
+++ updater/scripts/failed-webhooks.sh
@@ -4,19 +4,23 @@
 # List failed webhooks
 #
 
-echo -e "hook_id\ttype\thost\tmessage"
+echo -e "hook_id\ttype\thost\tmessage\tcount"
 
 zcat -f /var/log/hookshot/exceptions.log.1* |
   jq --slurp '.[] | del(.backtrace) | {hook_id,service_host,message,class,parent}' |
-  jq --slurp -c 'unique_by(.hook_id,.service_host,.class) | sort_by(.hook_id) | .[] | {id: .hook_id,url: .service_host, data: (.parent|tostring), msg: .message}' |
+  jq --slurp -c 'sort_by(.hook_id) | .[] | {id: .hook_id,url: .service_host, data: (.parent|tostring), msg: .message}' |
   # remove this part as it's not real JSON and breaks the remaining chain
-  sed -E 's/{\\\"url\\\"=>\\\"(.*)\\\"\}, //g' |
+  sed -e 's/{\\"url[^}]*}, //' |
   # this can probably be done more elegantly
   jq --slurp '.[] | {id: .id, url: .url, type: (.data|tostring|fromjson)[0], msg: .msg}' |
   jq --slurp -c '.[] | [.id,.url,.type,.msg]' |
+  sort |
+  uniq -ic |
+  sort -rn |
+  awk -F [ '{printf("%s,%s\n", $2,$1)}' |
   awk -F, '{
     gsub(/(\[|\]|\")/, "");
     gsub(/-[0-9]+/, "", $3);
-    printf("%s\t%s\t%s\t%s\n",$1,$3,$2,$4)
+    gsub(/ /, "", $5);
+    printf("%s\t%s\t%s\t%s\t%s\n",$1,$3,$2,$4,$5)
   }'
-
