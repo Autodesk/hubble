c76040aac587c56af2d734441bc68afcd72fcb3c
Stefan Stölzle <stoe@github.com>
2020-10-08 08:48:08 +0200

    🤘Delete webhook type suffix
    We don't want e.g. `business-1` but only `business` for the webhook type

--- updater/scripts/failed-webhooks.sh
+++ updater/scripts/failed-webhooks.sh
@@ -12,8 +12,8 @@ zcat -f /var/log/hookshot/exceptions.log.1* |
   # remove this part as it's not real JSON and breaks the remaining chain
   sed -e 's/{\\"url[^}]*}, //' |
   # this can probably be done more elegantly
-  jq -r -c --slurp '.[] | "\(.id)\t\((.data|tostring|fromjson)[0])\t\(.url)\t\(.msg)"' |
+  jq -r -c --slurp '.[] | "\(.id)\t\((.data|tostring|fromjson)[0] | sub("-[0-9]+"; ""))\t\(.url)\t\(.msg)"' |
   sort |
   uniq -ic |
   sort -rn |
-   perl -pne 's/([0-9]+)\s(.*)/\2\t\1/'
+  perl -pne 's/([0-9]+)\s(.*)/\2\t\1/'
