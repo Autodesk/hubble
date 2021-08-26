40ef61ed57748ce1f09b6837edccc4dd9d690a4a
Milos Svasek <milos@svasek.net>
2021-08-26 14:43:13 +0200

    Apply suggestions from code review
    Co-authored-by: Stefan Stölzle <stoe@github.com>

--- updater/scripts/git-protocol.sh
+++ updater/scripts/git-protocol.sh
@@ -4,7 +4,25 @@
 #
 echo -e "Git protocol\tconnections"
 
-zcat -f /var/log/syslog.1* |
+function ghe_greater_equal () {
+    cat /etc/github/enterprise-release |
+        perl -sne '
+            use version;
+            my ($installed) = $_ =~ /RELEASE_VERSION="([0-9]+([.][0-9]+)+)"/;
+            exit (version->parse($installed) lt version->parse($required));
+        ' -- -required="$1"
+    return $?
+}
+
+if ghe_greater_equal "3.0.0"; then
+    # check yesterday's log file post 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/syslog.1* | grep babeld"
+else
+    # check yesterday's log file pre 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/babeld/babeld.log.1*"
+fi
+
+eval "${CAT_LOG_FILE}" |
 	perl -ne 'print if s/.*proto=([^ ]+).*op done.*/\1/' |
 	sort |
 	uniq -c |
