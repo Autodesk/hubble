0e5d08d56b96438852d491c1220d84db891c9202
Stefan Stölzle <stoe@github.com>
2021-08-26 10:57:04 +0200

    Use GHES 3.x syntax for failed webhooks
    Signed-off-by: Stefan Stölzle <stoe@github.com>

--- updater/scripts/failed-webhooks.sh
+++ updater/scripts/failed-webhooks.sh
@@ -6,14 +6,37 @@
 
 echo -e "hook_id\ttype\thost\tmessage\tcount"
 
-zcat -f /var/log/hookshot/exceptions.log.1* |
-  jq --slurp '.[] | del(.backtrace) | {hook_id,service_host,message,class,parent}' |
-  jq --slurp -c 'sort_by(.hook_id) | .[] | {id: .hook_id,url: .service_host, data: (.parent|tostring), msg: .message}' |
-  # remove this part as it's not real JSON and breaks the remaining chain
-  sed -e 's/{\\"url[^}]*}, //' |
-  # this can probably be done more elegantly
-  jq -r -c --slurp '.[] | "\(.id)\t\((.data|tostring|fromjson)[0] | sub("-[0-9]+"; ""))\t\(.url)\t\(.msg)"' |
-  sort |
-  uniq -ic |
-  sort -rn |
-  perl -pne 's/([0-9]+)\s(.*)/\2\t\1/'
+function ghe_greater_equal () {
+    cat /etc/github/enterprise-release |
+        perl -sne '
+            use version;
+            my ($installed) = $_ =~ /RELEASE_VERSION="([0-9]+([.][0-9]+)+)"/;
+            exit (version->parse($installed) lt version->parse($required));
+        ' -- -required="$1"
+    return $?
+}
+
+if ghe_greater_equal "3.0.0" then
+    # check yesterday's log file post 3.0.0
+    zcat -f /var/log/syslog.1* | grep hookshot-go |
+        grep 'hook_id=[^ ]' |
+        grep -v 'status=200' |
+        perl -ne 'print if s/.*parent=([^ ]+)-.*hook_id=([^ ]+).*dest_url=([^ ]+).*public_error_message="(.*)".*/\2\t\1\t\3\t\4/' |
+        sort |
+        uniq -ic |
+        sort -rn |
+        perl -pne 's/([0-9]+)\s(.*)/\2\t\1/'
+else
+    # check yesterday's log file pre 3.0.0
+    zcat -f /var/log/hookshot/exceptions.log.1* |
+        jq --slurp '.[] | del(.backtrace) | {hook_id,service_host,message,class,parent}' |
+        jq --slurp -c 'sort_by(.hook_id) | .[] | {id: .hook_id,url: .service_host, data: (.parent|tostring), msg: .message}' |
+        # remove this part as it's not real JSON and breaks the remaining chain
+        sed -e 's/{\\"url[^}]*}, //' |
+        # this can probably be done more elegantly
+        jq -r -c --slurp '.[] | "\(.id)\t\((.data|tostring|fromjson)[0] | sub("-[0-9]+"; ""))\t\(.url)\t\(.msg)"' |
+        sort |
+        uniq -ic |
+        sort -rn |
+        perl -pne 's/([0-9]+)\s(.*)/\2\t\1/'
+fi
