8afc6d8e909ce92e97d827a5bdb03010fe0ef901
Stefan Stölzle <stoe@github.com>
2021-08-26 10:57:29 +0200

    Use GHES 3.x syntax for Git protocol usage
    Signed-off-by: Stefan Stölzle <stoe@github.com>

--- updater/scripts/git-protocol.sh
+++ updater/scripts/git-protocol.sh
@@ -4,8 +4,26 @@
 #
 echo -e "Git protocol\tconnections"
 
-zcat -f /var/log/babeld/babeld.log.1* |
-	perl -ne 'print if s/.*proto=([^ ]+).*op done.*/\1/' |
-	sort |
-	uniq -c |
-	awk '{printf("%s\t%s\n",$2,$1)}'
+function ghe_greater_equal () {
+    cat /etc/github/enterprise-release |
+        perl -sne '
+            use version;
+            my ($installed) = $_ =~ /RELEASE_VERSION="([0-9]+([.][0-9]+)+)"/;
+            exit (version->parse($installed) lt version->parse($required));
+        ' -- -required="$1"
+    return $?
+}
+
+if ghe_greater_equal "3.0.0"; then
+    # check yesterday's log file post 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/syslog.1* | grep babeld"
+else
+    # check yesterday's log file pre 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/babeld/babeld.log.1*"
+fi
+
+eval "${CAT_LOG_FILE}" |
+    perl -ne 'print if s/.*proto=([^ ]+).*op done.*/\1/' |
+    sort |
+    uniq -c |
+    awk '{printf("%s\t%s\n",$2,$1)}'
