9ff2ea827560ba3d3fd4ec2400bcd8642fe110b7
Stefan Stölzle <stoe@github.com>
2021-08-26 10:57:43 +0200

    Use GHES 3.x syntax for Git requests
    Signed-off-by: Stefan Stölzle <stoe@github.com>

--- updater/scripts/git-requests.sh
+++ updater/scripts/git-requests.sh
@@ -4,7 +4,25 @@
 #
 echo -e "repository\tsource IP\trequests"
 
-zcat -f /var/log/babeld/babeld.log.1* |
+function ghe_greater_equal () {
+    cat /etc/github/enterprise-release |
+        perl -sne '
+            use version;
+            my ($installed) = $_ =~ /RELEASE_VERSION="([0-9]+([.][0-9]+)+)"/;
+            exit (version->parse($installed) lt version->parse($required));
+        ' -- -required="$1"
+    return $?
+}
+
+if ghe_greater_equal "3.0.0"; then
+    # check yesterday's log file post 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/syslog.1* | grep babeld"
+else
+    # check yesterday's log file pre 3.0.0
+    CAT_LOG_FILE="zcat -f /var/log/babeld/babeld.log.1*"
+fi
+
+eval "${CAT_LOG_FILE}" |
     perl -ne 'print if s/.*ip=([^ ]+).*repo=([^ ]+).*/\1 \2/' |
     sort |
     uniq -ic |
