51d7c11fca439eea89cf4d348291cded6761cb25
Patrick Lühne <patrick@luehne.de>
2022-03-04 12:57:45 +0100

    Minor formatting

--- updater/scripts/git-requests.sh
+++ updater/scripts/git-requests.sh
@@ -5,8 +5,8 @@
 echo -e "repository\tsource IP\trequests"
 
 zcat -f /var/log/babeld/babeld.log.1* |
-    perl -ne 'print if s/.*ip=([^ ]+).*repo=([^ ]+).*/\1 \2/' |
-    sort |
-    uniq -ic |
-    sort -rn |
-    awk '{printf("%s\t%s\t%s\n",$3,$2,$1)}'
+	perl -ne 'print if s/.*ip=([^ ]+).*repo=([^ ]+).*/\1 \2/' |
+	sort |
+	uniq -ic |
+	sort -rn |
+	awk '{printf("%s\t%s\t%s\n",$3,$2,$1)}'
